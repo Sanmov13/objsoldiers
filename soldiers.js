39e29e515db513d5d55169105929876c5c443ce2
Sanmov13 <105623098+Sanmov13@users.noreply.github.com>
2022-07-12 14:59:07 +0300

    Add files via upload
    Задал начальные значения

---
+++ soldiers.js
@@ -0,0 +1,19 @@
+const soldier = {
+    name: 'Rambo',
+    health: 10,
+    weapon: {
+        nameWeapon: 'Minimet',
+        patrons: 30
+    },
+    magazines: 3,
+    shoot: function() {
+
+    },
+    reload: function() {
+
+    },
+    hurt: function() {
+        
+    }
+
+}
